393f67ce4b193cada6d7bb67b6ef9454ef6e2c84
trungkien1208 <kien@tagglehealth.com>
2023-09-01 20:05:48 +0700

    no message

--- src/components/home/mentors.data.ts
+++ src/components/home/mentors.data.ts
@@ -5,48 +5,49 @@ export const data: Array<Mentor> = [
     id: 1,
     photo: '/images/mentors/christian-buehner-DItYlc26zVI-unsplash.jpg',
     name: 'Jhon Dwirian',
-    category: 'UI/UX Design',
+    category: 'Quản Lý Nhà Hàng',
     description:
-      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'Trà Sữa Boh Teh Tarik là một tuyệt phẩm hương vị từ Malaysia. Với hương thơm đặc trưng của trà đen và độ sánh đỉnh cao của sữa, món trà này thực sự đã chinh phục trái tim của tôi. Sự kết hợp hoàn hảo này tạo nên một trải nghiệm độc đáo và không thể nhầm lẫn.',
     company: {
-      name: 'Grab',
-      logo: '/images/companies/grab.png',
+      name: 'shopee',
+      logo: '/images/companies/shopee.svg',
     },
   },
   {
     id: 2,
     photo: '/images/mentors/jonas-kakaroto-KIPqvvTOC1s-unsplash.jpg',
     name: 'Leon S Kennedy',
-    category: 'Machine Learning',
+    category: 'Đầu Bếp Nhà Hàng',
     description:
-      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'Trà Sữa Boh Teh Tarik không chỉ là một đồ uống mà còn là một tác phẩm nghệ thuật hương vị. Hương thơm đặc trưng và sự kết hợp độc đáo của trà và sữa đã tạo nên một trải nghiệm thú vị cho vị giác. Tôi không thể không thử món trà này mỗi khi có cơ hội.',
     company: {
-      name: 'Google',
-      logo: '/images/companies/google.png',
+      name: 'tiktok',
+      logo: '/images/companies/tiktok.svg',
     },
   },
   {
     id: 3,
     photo: '/images/mentors/noah-buscher-8A7fD6Y5VF8-unsplash.jpg',
-    name: 'Nguyễn Thuy',
-    category: 'Android Development',
+    name: 'Nguyễn Thụy Vy',
+    category: 'Quản Lý Marketing Nhà Hàng',
     description:
-      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'Trà Sữa Boh Teh Tarik là một hiệp hội nơi bạn có thể trải nghiệm hương vị tinh tế và độc đáo của trà và sữa. Sự kết hợp hoàn hảo này đã tạo nên một cơ hội tuyệt vời cho các nhà hàng và quán trà sữa phát triển kinh doanh.',
     company: {
-      name: 'Airbnb',
-      logo: '/images/companies/airbnb.png',
+      name: 'shopee',
+      logo: '/images/companies/shopee.svg',
     },
   },
   {
     id: 4,
     photo: '/images/mentors/philip-martin-5aGUyCW_PJw-unsplash.jpg',
     name: 'Rizki Known',
-    category: 'Fullstack Development',
+    category: 'Tư Vấn Thực Phẩm & Đồ Uống',
     description:
-      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'Tôi đã du lịch rất nhiều nơi và thử nhiều loại đồ uống khác nhau, nhưng Trà Sữa Boh Teh Tarik vẫn là món yêu thích của tôi. Đây là một loại thức uống không thể thiếu cho mọi người, đặc biệt là trong ngành công nghiệp thực phẩm và đồ uống.',
     company: {
-      name: 'Microsoft',
-      logo: '/images/companies/microsoft.png',
+      name: 'tiktok',
+      logo: '/images/companies/tiktok.svg',
     },
   },
+  // Add more mentors with different job titles here
 ]
